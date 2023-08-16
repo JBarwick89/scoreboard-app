cdd56f1fad3742c20d7098abb8e79bca43a5459a
josh barwick <jbarwick@quantumedgetechnology.com>
2023-08-15 19:29:22 -0500

    clean up fetchGamesData

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import './App.css';
+import Scoreboard from './Scoreboard';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
   const [Games, setGames] = useState([]);
-
+  const sportIdMapping = {
+    1: 'Basketball',
+    2: 'Football',
+    3: 'Baseball',
+    4: 'Softball',
+    5: 'Lacrosse',
+    6: 'Soccer',
+    7: 'Volleyball',
+    8: 'Field Hockey',
+    9: 'Ice Hockey',
+    10: 'Water Polo',
+    11: 'Cross Country',
+    12: 'Golf',
+    13: 'Tennis',
+  };
+    
   useEffect(() => {
-    const getGames = async () => {
+    fetchGamesData();
+    // console.log(Games);
+  }, []);
+
+  const fetchGamesData = async () => {
+    try {
       const { data } = await axios.get('/games', {
-        // headers: {
-        //   'Access-Control-Allow-Origin': '*',
-        // }, 
-        // withCredentials: false,
         params: {
-          // limit: 1,
-          date: '2022-05-12',
+          date: '2022-05-12', // TODO: make user controllable
           priority_order: true,
         },
       });
 
-      console.log(data); //TODO: REMOVE
-      return data;
+      setGames(data.data);
+    } catch (error) {
+      console.error('Error fetching games data:', error);
     }
-  
-    setGames(getGames());
-  }, []);
+  }
 
   return (
     <div className="App">
-      
+      {Games.length > 0 ? <Scoreboard games={Games} /> : null}
     </div>
   );
 }
